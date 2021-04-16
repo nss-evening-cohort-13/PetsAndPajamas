de688008a8bcb3e304059c3aa4fd04bf4d351975
Robert Bandstra <rob.bandstra@gmail.com>
2021-04-16 18:58:00 -0500

    added method to get all customer orders

--- DataAccess/CustomerOrdersRepository.cs
+++ DataAccess/CustomerOrdersRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PetsAndPajamas.Models;
 using Dapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
@@ -15,6 +16,29 @@ namespace PetsAndPajamas.DataAccess
         public CustomerOrdersRepository(IConfiguration config)
         {
             ConnectionString = config.GetConnectionString("PetsAndPajamas");
+        }
+
+        public IEnumerable<CustomerOrder> GetAll()
+        {
+            var sql = @"select * from CustomerOrder co
+                            left join SiteUser su
+                                on su.Id = co.UserId
+                            left join PaymentType pt
+                                on pt.Id = co.PaymentId
+                            join ShoppingCart sc
+                                on sc.Id = su.CartId";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            var orders = db.Query<CustomerOrder, User, PaymentType, CustomerOrder>(sql,
+                (customerOrder, user, paymentType) =>
+                {
+                    customerOrder.User = user;
+                    customerOrder.PaymentType = paymentType;
+
+                    return customerOrder;
+                }, splitOn: "Id");
+            return orders;
         }
     }
 }
