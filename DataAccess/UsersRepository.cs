2a792c696e46d672151b42afcd0911512128e648
Dana Pham <danapham12@gmail.com>
2021-04-16 17:21:20 -0500

    created get single user call

--- DataAccess/UsersRepository.cs
+++ DataAccess/UsersRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using PetsAndPajamas.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +11,29 @@ namespace PetsAndPajamas.DataAccess
     public class UsersRepository
     {
         const string ConnectionString = "Server=localhost;Database=LoafAndStranger;Trusted_Connection=True;";
+
+        public List<User> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"Select *
+                        From SiteUser";
+
+            return db.Query<User>(sql).ToList();
+        }
+
+        public User Get(int id)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"select *
+                        from SiteUser
+                        where id = @id";
+
+            var user = db.QueryFirstOrDefault<User>(sql, new { id });
+
+            return user;
+        }
 
     }
 }
