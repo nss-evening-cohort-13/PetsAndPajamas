7fe0ac86338c5181097ee0fd6eacd276260a4c3e
Dana Pham <danapham12@gmail.com>
2021-04-16 20:49:55 -0500

    pet type gets completed

---
+++ Controllers/PetTypesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetsAndPajamas.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetsAndPajamas.Controllers
+{
+    [Route("api/PetTypes")]
+    [ApiController]
+    public class PetTypesController : Controller
+    {
+        PetTypesRepository _repo;
+        public PetTypesController()
+        {
+            _repo = new PetTypesRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPetTypes()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var type = _repo.Get(id);
+
+            if (type == null)
+            {
+                return NotFound("This pet type id does not exist");
+            }
+
+            return Ok(type);
+        }
+    }
+}
---
+++ DataAccess/PetTypesRepository.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using PetsAndPajamas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetsAndPajamas.DataAccess
+{
+    public class PetTypesRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=PetsAndPajamas;Trusted_Connection=True;";
+        public List<PetType> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"Select *
+                        from PetType";
+
+            return db.Query<PetType>(sql).ToList();
+        }
+
+        public PetType Get(int id)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"Select *
+                        from PetType
+                        where id = @id";
+
+            return db.QueryFirstOrDefault<PetType>(sql, new { id });
+        }
+    }
+}
