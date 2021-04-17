52a53cbd7ce78b5df820e4ce18ba9d69f5bc10c0
Wendell Patton Jr <wendell.pattonjr@gmail.com>
2021-04-16 22:42:17 -0500

    gets all payment types and gets payment type by id

--- Controllers/PaymentTypesController.cs
+++ Controllers/PaymentTypesController.cs
@@ -4,12 +4,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PetsAndPajamas.DataAccess;
 
 namespace PetsAndPajamas.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/PaymentTypes")]
     [ApiController]
     public class PaymentTypesController : ControllerBase
     {
+        PaymentTypesRepository _repo;
+
+        public PaymentTypesController()
+        {
+            _repo = new PaymentTypesRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPaymentTypes()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetById(int id)
+        {
+            var type = _repo.Get(id);
+
+            if (type == null)
+            {
+                return NotFound("This payment type id does not exist");
+            }
+
+            return Ok(type);
+        }
     }
 }
+
--- DataAccess/PaymentTypesRepository.cs
+++ DataAccess/PaymentTypesRepository.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PetsAndPajamas.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
 namespace PetsAndPajamas.DataAccess
 {
     public class PaymentTypesRepository
     {
+        const string ConnectionString = "Server=localhost;Database=PetsAndPajamas;Trusted_Connection=True;";
+
+        public List<PaymentType> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"select *
+                        from PaymentType";
+
+            return db.Query<PaymentType>(sql).ToList();
+        }
+
+        public PaymentType Get(int id)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"select *
+                        from PaymentType
+                        where id = @id";
+
+            var type = db.QueryFirstOrDefault<PaymentType>(sql, new { id });
+
+            return type;
+        }
+
+
     }
 }
