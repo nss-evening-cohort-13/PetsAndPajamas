eb76de5d3cc3b8b1649d8956ba55d5779ca54306
Dana Pham <danapham12@gmail.com>
2021-04-16 20:07:47 -0500

    saving to checkout PR

--- DataAccess/PajamaTypesRepository.cs
+++ DataAccess/PajamaTypesRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using PetsAndPajamas.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,29 @@ namespace PetsAndPajamas.DataAccess
 {
     public class PajamaTypesRepository
     {
+        const string ConnectionString = "Server=localhost;Database=PetsAndPajamas;Trusted_Connection=True;";
+
+        public List<PajamaType> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"select *
+                        from PajamaType";
+
+            return db.Query<PajamaType>(sql).ToList();
+        }
+
+        public PajamaType Get(int id)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"select *
+                        from PajamaType
+                        where id = @id";
+
+            var type = db.QueryFirstOrDefault(sql, new { id });
+
+            return type;
+        }
     }
 }
