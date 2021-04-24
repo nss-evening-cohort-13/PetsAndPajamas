e05db6e084c2f84401561e766f58d9612b167ef4
Dana Pham <danapham12@gmail.com>
2021-04-24 10:52:16 -0500

    images and header are showing up

--- PetsAndPajamas.ui/src/views/Home.js
+++ PetsAndPajamas.ui/src/views/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-export default function Home() {
-  return (
-        <div>
-            <h1>Home</h1>
-        </div>
-  );
+export default class Home extends React.Component {
+  render() {
+    return (
+      <>
+      <h1 className="home-header">
+        Hello fellow pet lovers! Welcome to Pets & Pajamas where we provide high-quality sleepwear for felines and canines.
+      </h1>
+    <div>
+      <img src="https://i.imgur.com/D8NHcBA.png" alt="pets wearing pajamas"></img>
+    </div>
+    </>
+    );
+  }
 }
