4d0a95dd414900b2d770b333acaa2da9991bf40f
Dana Pham <67748110+danapham@users.noreply.github.com>
2021-04-27 18:22:39 -0500

    Update Checkout.js

--- PetsAndPajamas.ui/src/views/Checkout.js
+++ PetsAndPajamas.ui/src/views/Checkout.js
@@ -6,26 +6,6 @@ export default class Checkout extends React.Component {
     return (
       <div>
           <h1>Checkout</h1>
-          <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
       </div>
     );
   }
