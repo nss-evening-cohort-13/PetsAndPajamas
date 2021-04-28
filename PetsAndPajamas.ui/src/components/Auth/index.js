69c67582614237375b6dea5c3b1dc6f6d4adf6b0
bandstrar <rob.bandstra@gmail.com>
2021-04-27 19:24:35 -0500

    added login and posting user info to database

--- PetsAndPajamas.ui/src/components/Auth/index.js
+++ PetsAndPajamas.ui/src/components/Auth/index.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import axios from 'axios';
+import { baseUrl } from '../../helpers/config.json';
 
 class Auth extends Component {
   loginClickEvent = (e) => {
     e.preventDefault();
     const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  }
+    firebase.auth().signInWithPopup(provider).then((cred) => {
+      const user = cred.additionalUserInfo.profile;
+      if (cred.additionalUserInfo.isNewUser) {
+        // get uid from firebase, the rest of this info is just made up, whatever registration information you're already saving to the database
+        const userInfo = {
+          firstname: user.given_name,
+          lastname: user.family_name,
+          emailaddress: user.email,
+          admin: false,
+          isactive: true
+          // cred here is the created, logged in user from firebase
+        };
+        // save the user to the the api
+        axios.post(`${baseUrl}/siteusers`, userInfo);
+      }
+    });
+  };
 
   render() {
     return (
--- PetsAndPajamas.ui/src/components/MyNavbar/index.js
+++ PetsAndPajamas.ui/src/components/MyNavbar/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import {
@@ -9,63 +8,7 @@ import {
   NavItem,
 } from 'reactstrap';
 import SearchInput from '../SearchInput';
-
-class MyNavbar extends Component {
-  state = {
-    isOpen: false,
-  }
-
-  toggle = () => this.setState({
-    isOpen: !this.state.isOpen,
-  })
-
-  render() {
-    const { isOpen } = this.state;
-
-    return (
-      <>
-        <div>
-        <Navbar color='dark' dark expand='md' className='navbar justify-content-between'>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className='mr-auto' navbar>
-              <NavItem>
-                <Link className="nav-link" to='/about'>About</Link>
-              </NavItem>
-              <Link className="nav-link" to='/'>Home</Link>
-              <NavItem>
-                <Link className="nav-link" to='/dog-store'>
-                  Dogs
-                </Link>
-              </NavItem>
-              <NavItem>
-                <Link className="nav-link" to='/cat-store'>
-                  Cats
-                </Link>
-              </NavItem>
-            </Nav>
-            <p className='mr-2 mt-3 text-light'>Search:</p>
-            <SearchInput />
-          </Collapse>
-        </Navbar>
-      </div>
-        </>
-    );
-  }
-}
-
-export default withRouter(MyNavbar);
-=======
-import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  Nav,
-  NavItem,
-} from 'reactstrap';
-import SearchInput from '../SearchInput';
+import Auth from '../Auth';
 
 class MyNavbar extends Component {
   state = {
@@ -106,6 +49,7 @@ class MyNavbar extends Component {
             </Nav>
             <p className='mr-3 mt-3 text-light'>Search:</p>
             <SearchInput />
+            <Auth />
           </Collapse>
         </Navbar>
       </div>
@@ -115,4 +59,3 @@ class MyNavbar extends Component {
 }
 
 export default withRouter(MyNavbar);
->>>>>>> development
