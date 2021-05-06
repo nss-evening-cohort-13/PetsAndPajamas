f7c142181ec37c87e699aa85075d475673fd6ce6
bandstrar <rob.bandstra@gmail.com>
2021-05-05 20:08:00 -0500

    added sidebar components

---
+++ PetsAndPajamas.ui/src/components/Sidebar/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+// import { Link, withRouter } from 'react-router-dom';
+// import {
+//   Collapse,
+//   Navbar,
+//   NavbarToggler,
+//   Nav,
+//   NavItem,
+//   NavbarBrand
+// } from 'reactstrap';
+
+class Sidebar extends Component {
+  state = { }
+
+  render() {
+    return (
+<h1>navvo</h1>
+    );
+  }
+}
+
+export default Sidebar;
