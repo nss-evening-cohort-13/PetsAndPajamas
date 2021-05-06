1b1c0cb72393cf99b7f6b2f3bd90abb64e235ee7
Wendell Patton Jr <wendell.pattonjr@gmail.com>
2021-05-05 20:48:21 -0500

    total inventory dashboard is now showing all inventory items

--- PetsAndPajamas.ui/src/views/AdminProducts.js
+++ PetsAndPajamas.ui/src/views/AdminProducts.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
+import { Table } from 'reactstrap';
+import pajamaData from '../helpers/data/pajamaData';
+import TotalInventory from '../components/TotalInventory';
 
 class AdminProducts extends Component {
-  state = { }
+  state = {
+    pajamas: [],
+  }
+
+  componentDidMount() {
+    this.getAllPajamaInventory();
+  }
+
+  getAllPajamaInventory = () => {
+    pajamaData.getAllPajamas()
+      .then((response) => this.setState({
+        pajamas: response,
+      }));
+  }
 
   render() {
+    const { pajamas } = this.state;
+
+    const renderPajamas = () => (
+      pajamas.map((pajama) => (
+        <TotalInventory key={pajama.id} pajama={pajama} image={pajama.image} title={pajama.title} inventory={pajama.inventory} />
+      ))
+    );
+
     return (
-      <div>
-        <h1>
-          Admin Products
-        </h1>
+      <div className='table-of-pajama-inventory'>
+        <Table boardered>
+          <tbody>
+            <tr>
+              <td>
+                {pajamas.length
+                  ? renderPajamas()
+                  : 'There are no pajamas in inventory.'}
+              </td>
+            </tr>
+          </tbody>
+        </Table>
       </div>
     );
   }
