76aa3c6b2137b4bd2d416c547dc6c0cfc04cfc60
bandstrar <rob.bandstra@gmail.com>
2021-05-10 19:25:58 -0500

    added sidebar to profile page

--- PetsAndPajamas.ui/src/views/ProfilePage.js
+++ PetsAndPajamas.ui/src/views/ProfilePage.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import UserSidebar from '../components/UserSidebar';
 
 class ProfilePage extends Component {
   state = { }
 
   render() {
     return (
+      <>
+       <Container fluid>
+                <Row>
+                    <Col xs={-1} id="sidebar-wrapper">
+                      <UserSidebar />
+                    </Col>
+                    <Col xs={0} id="page-content-wrapper">
       <h1>Profile Page</h1>
+      </Col>
+                </Row>
+            </Container>
+      </>
     );
   }
 }
